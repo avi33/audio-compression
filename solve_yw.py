32c6fa6cfdadaf8cfe77d37b336820fa6e057030
avi <avi.gazneli@gmail.com>
2024-01-06 11:13:22 +0200

    update yw

--- solve_yw.py
+++ solve_yw.py
@@ -1,44 +1,52 @@
+from typing import Any
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import numpy as np
 
-class YullieWalker(nn.Module):
-    def __init__(self, p=None) -> None:
-        super().__init__()
-        self.p = p
-        self.step = 1                
+
+class YullieWalker:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def build_system_of_equantions(x, win_len, step=1):
+        y = x[win_len::step]
+        X = x[:-step].unfold(dimension=0, size=win_len, step=step).flip(dims=[-1, ])    
+        return X, y    
     
-    def build_system_of_equantions(self, x):
-        y = x[win_len::self.step]
-        X = x[:-self.p].unfold(dimension=0, size=self.p, step=self.step)
-        return X, y
+    @staticmethod
+    def solve_yw(X, y):
+        A = torch.matmul(X.T, X)
+        b = torch.matmul(X.T, y)
+        theta = torch.linalg.lstsq(A, b).solution        
+        return theta   
     
-    def forward(self, x):
-        X, y = self.build_system_of_equantions(x)
-        return X, y
+    @staticmethod
+    def est_ar_params(x, win_len, step):
+        R, r = YullieWalker.build_system_of_equantions(x=x, win_len=win_len, step=step)
+        theta = YullieWalker.solve_yw(R, r)
+        y_pred = torch.matmul(R, theta)
+        y_pred = torch.cat((x[:win_len], y_pred))
+        return y_pred, theta
         
 
-def build_system_of_equantions(x, win_len, step=1):
-    y = x[win_len::step]
-    X = x[:-step].unfold(dimension=0, size=win_len, step=step)    
-    return X, y
-
-def yw(X, y, p=1):
-    win_len = p    
-    X = X.flip(dims=[-1, ])
-    p = torch.linalg.solve(torch.matmul(X.T, X), torch.matmul(X.T, y))
-    y_pred = torch.matmul(X, p)
-    return y_pred, p
-
 if __name__ == "__main__":
-    win_len = 128
-    x = torch.randn(100).float()
-    X, y = build_system_of_equantions(x, step=1, win_len=4)    
-    y_pred, p = yw(X, y, p=win_len)
-    err_rel = 20*torch.log10((y_pred-y).abs().mean()/y.abs().mean())
-    err_abs = (y_pred-y).abs()
-    plt.plot(y)
-    plt.plot(y_pred, 'r-')
-    plt.title("rel err:{:.2f}".format(err_rel))
+    fs = 200
+    f = 12
+    t = torch.arange(0, 1, 1/fs).float()
+    x = torch.sin(2*np.pi*f*t)
+    std_noise = 0.1
+    y = x + torch.randn_like(x)*std_noise
+    y_pred, theta = YullieWalker.est_ar_params(y, win_len=3, step=1)
+    # X, y = build_system_of_equantions(x, step=1, win_len=3)    
+    # y_pred, p = yw(X, y, p=3)    
+    err = y_pred-y
+    err_rel = 20*torch.log10(err.abs().mean()/y.abs().mean())
+    err_abs = 20*torch.log10(err.abs().mean())
+    print(20*np.log10(std_noise))
+    plt.plot(y, '.-')
+    plt.plot(y_pred, 'ro-')        
+    plt.title("rel err:{:.2f} abs err:{:.2f}".format(err_rel, err_abs))
     plt.grid(True)
-    plt.show()
+    plt.show()    
